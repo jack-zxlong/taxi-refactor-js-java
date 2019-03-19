ada390adf5438ca5072594149aee868ea8464585
livingwei <929887558@qq.com>
2019-03-19 10:28:12 +0800

    liweiiSecondCommit

--- js/src/Receipt.js
+++ js/src/Receipt.js
@@ -1,32 +1,34 @@
-const FIXED_CHARGE = 50; // 固定电荷
-const PEAK_TIME_MULTIPLIER = 1.2; // 峰值时间乘数
-const OFF_PEAK_MULTIPLIER = 1.0; // 非峰值时间乘数
-const RATE_CHANGE_DISTANCE = 10; // 发生收费梯度变化的值
-const PRE_RATE_CHANGE_NON_AC_RATE = 15; // 没有空调RATE_CHANGE_DISTANCE值之前的每公里收费
-const POST_RATE_CHANGE_NON_AC_RATE = 12; // 没有空调RATE_CHANGE_DISTANCE值之后的每公里收费
-const PRE_RATE_CHANGE_AC_RATE = 20; // 有空调RATE_CHANGE_DISTANCE值之前的每公里收费
-const POST_RATE_CHANGE_AC_RATE = 17; // 有空调RATE_CHANGE_DISTANCE值之后的每公里收费
-const SALES_TAX_RATE = 0.1; // 销售税税率
+var FIXED_CHARGE = 50,
+PEAK_TIME_MULTIPLIER = 1.2,
+PEAK_TIME_MULTIPLIER = 1.2,
+OFF_PEAK_MULTIPLIER = 1.0,
+RATE_CHANGE_DISTANCE = 10,
+PRE_RATE_CHANGE_NON_AC_RATE = 15,
+POST_RATE_CHANGE_NON_AC_RATE = 12,
+PRE_RATE_CHANGE_AC_RATE = 20,
+POST_RATE_CHANGE_AC_RATE = 17,
+SALES_TAX_RATE = 0.1;
 
 class Receipt {
   constructor(taxi) {
     this.taxi = taxi;
   }
-
+  getIsAcPreCharges(isAirConditioned) {
+    return isAirConditioned ? PRE_RATE_CHANGE_AC_RATE : PRE_RATE_CHANGE_NON_AC_RATE
+  }
+  getIsAcPostCharges(isAirConditioned) {
+    return isAirConditioned ? POST_RATE_CHANGE_AC_RATE : POST_RATE_CHANGE_NON_AC_RATE
+  }
   getTotalCost() {
     let totalCost = 0;
     // fixed charges
     totalCost += FIXED_CHARGE;
 
     // taxi charges
-    let totalKms = this.totalKms;
-    // 是不是乘车高峰期
-    let peakTimeMultiple = this.peakTime ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
-    let isAirConditioned = this.airConditioned ? PRE_RATE_CHANGE_AC_RATE : PRE_RATE_CHANGE_NON_AC_RATE
-    let isPostAirConditioned = this.airConditioned ? POST_RATE_CHANGE_AC_RATE : POST_RATE_CHANGE_NON_AC_RATE
-    totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * isAirConditioned * peakTimeMultiple;
-    totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * isPostAirConditioned * peakTimeMultiple;
-
+    let totalKms = this.taxi.getTotalKms();
+    let peakTimeMultiple = this.taxi.isPeakTime() ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
+    totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * this.getIsAcPreCharges(this.taxi.isAirConditioned()) * peakTimeMultiple;
+    totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * this.getIsAcPostCharges(this.taxi.isAirConditioned()) * peakTimeMultiple;
     return totalCost * (1 + SALES_TAX_RATE);
   }
 }
--- js/test/Receipt.spec.js
+++ js/test/Receipt.spec.js
@@ -5,21 +5,21 @@ import {expect} from 'chai'
 describe('CashRegister', () => {
   it('shouldCalculateChargesForACTaxiFor30Kms', () => {
     const cost = new Receipt(new Taxi(true, 30, false)).getTotalCost();
-    expect(cost).to.be.closeTo(649);
+    expect(cost).to.be.closeTo(649, 0.01);
   });
 
   it('shouldCalculateChargesForNonACTaxiFor30Kms', () => {
     const cost = new Receipt(new Taxi(false, 30, false)).getTotalCost();
-    expect(cost).to.be.closeTo(484);
+    expect(cost).to.be.closeTo(484, 0.01);
   });
 
   it('shouldCalculateForACChargesFor30KmsPeakTime', () => {
     const cost = new Receipt(new Taxi(true, 30, true)).getTotalCost();
-    expect(cost).to.be.closeTo(767.8);
+    expect(cost).to.be.closeTo(767.8, 0.01);
   });
 
   it('shouldCalculateChargesForNonACTaxiFor30KmsPeakTime', () => {
     const cost = new Receipt(new Taxi(false, 30, true)).getTotalCost();
-    expect(cost).to.be.closeTo(569.8);
+    expect(cost).to.be.closeTo(569.8, 0.01);
   });
 })
